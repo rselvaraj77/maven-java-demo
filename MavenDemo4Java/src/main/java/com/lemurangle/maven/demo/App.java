26b9662e46e8e5165fc0b80045337c0718734fa8
Ramesh Selvaraj <ramesh.selvaraj@in.ibm.com>
2018-08-04 14:37:44 +0530

    Demo maven for simple java app

---
+++ MavenDemo4Java/src/main/java/com/lemurangle/maven/demo/App.java
@@ -0,0 +1,13 @@
+package com.lemurangle.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
